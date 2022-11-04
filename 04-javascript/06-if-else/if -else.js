807e42574078671c18145778e378272a956681e3
Rizki Prastyo <rizkiprastyo22@gmail.com>
2022-11-04 09:49:02 +0700

    [ADD] string, array, dan if-else

---
+++ 04-javascript/06-if-else/if -else.js
@@ -0,0 +1,15 @@
+let penentu = 2;
+
+// menampilkan jika sesuai kondisi
+if(penentu === 1){
+    console.log('satu');
+}
+
+else if(penentu === 2){
+    console.log('dua')
+}
+
+// menampilkan jika tidak ada kondisi yang benar
+else{
+    console.log('bukan satu')
+}
